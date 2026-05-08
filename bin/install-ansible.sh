315a0d2cc1eae4863be39d8f4005d9de84baaf1f
Patricio Rojas <projaso43@gmail.com>
2026-05-08 19:12:50 -0400

    chore: Update `install-ansible.sh` to include additional dependencies and Molecule plugins setup

--- bin/install-ansible.sh
+++ bin/install-ansible.sh
@@ -6,11 +6,16 @@ set -euo pipefail          # Terminate on error, avoid using uninitialized varia
 
 PYTHON_VERSION="3.12"
 echo "installing ansible  ... "
-sudo apt install ansible 
+sudo apt install ansible python3 python3-pip sshpass libssl-dev git
 
 uv init
 
 uv python install "$PYTHON_VERSION"
 uv python pin "$PYTHON_VERSION"
 
-uv add ansible-core ansible-dev-tools ansible-lint
+uv add ansible-core ansible-dev-tools ansible-lint yamllint pytest-testinfra
+python3 -m pip install --user molecule ansible-lint
+
+# Other plugins are:
+# containers gce docker vagrant podman openstack default ec2 azure
+python3 -m pip install --user "molecule-plugins[docker]"
